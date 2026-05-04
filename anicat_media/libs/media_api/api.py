79e6143760d0b429cb358b7bf81dd5def6cc5717
Thomas <thomas.rwubben@outlook.com>
2026-05-04 13:38:31 +0200

    fix

--- anicat_media/libs/media_api/api.py
+++ anicat_media/libs/media_api/api.py
@@ -46,8 +46,8 @@ def create_api_client(client_name: str, config: AppConfig) -> BaseApiClient:
     
     # Inject the parent config so the client can access general settings if needed
     try:
-        setattr(scoped_config, "parent", config)
-    except (AttributeError, TypeError):
+        object.__setattr__(scoped_config, "parent", config)
+    except (AttributeError, TypeError, ValueError):
         # In case the model is frozen or doesn't support attribute assignment
         pass
 
